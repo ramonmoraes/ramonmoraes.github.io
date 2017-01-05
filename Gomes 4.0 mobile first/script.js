460a594c1e51c7eb0a8895974ee73da11a8157b3
ramonmoraes <ramon.moraes94@gmail.com>
2017-01-05 14:41:03 -0300

    Add G&M

---
+++ Gomes 4.0 mobile first/script.js
@@ -0,0 +1,8 @@
+function slidedown(){
+  document.getElementsByClassName('grupo')[1].style.display="block";
+  document.getElementsByClassName('butoes')[0].style.display="none";
+}
+function slideup(){
+  document.getElementsByClassName('grupo')[1].style.display="none";
+  document.getElementsByClassName('butoes')[0].style.display="initial";
+}
